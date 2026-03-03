2d80c62310069d48dd65b802d6265c7535806cde
Yaswanth <kambhampatiyashu1708@gmail.com>
2026-03-03 12:16:02 +0530

    Adding frontend url

--- config/settings.py
+++ config/settings.py
@@ -146,3 +146,6 @@ ALLOWED_HOSTS = ["*"]  # later replace with render domain
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://http://localhost:3000.onrender.com",
+]
